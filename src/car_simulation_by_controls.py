12f5ba7a4d544023b830be37636d1fefe1bffa0f
Azeem Bande-Ali <me@azeemba.com>
2020-07-11 22:33:56 -0400

    Forward and turning simulation doing alright!

---
+++ src/car_simulation_by_controls.py
@@ -0,0 +1,73 @@
+"""
+Module to do rough car simulation based on SimpleControllerState
+"""
+from dataclasses import dataclass
+
+from rlbot.utils.game_state_util import Rotator
+from rlbot.agents.base_agent import SimpleControllerState
+
+from util.vec import Vec3
+from util.orientation import Orientation
+
+@dataclass
+class SimPhysics():
+    location: Vec3
+    velocity: Vec3
+    angular_velocity: Vec3
+    rotation: Rotator
+
+def step(physics: SimPhysics, controls: SimpleControllerState, dt: float):
+
+    # Start by assuming on ground. Will adjust things in the future
+    steer = controls.steer
+    radians_per_sec = 0
+    if abs(controls.throttle) > 0.01:
+        # Average values taken by graphing it out
+        radians_per_sec = 2.42
+        if controls.boost and controls.handbrake:
+            radians_per_sec = 4.5
+        elif controls.handbrake:
+            radians_per_sec = 5
+        elif controls.boost:
+            radians_per_sec = 2.05
+
+    yaw = physics.rotation.yaw
+    physics.rotation.yaw = yaw + radians_per_sec*steer*dt
+
+    orientation = Orientation(physics.rotation)
+    orientation.forward.z = 0
+    direction = orientation.forward.dot(physics.velocity) < 0
+    if direction == 0:
+        direction = 1
+    direction_float_normalized = direction/abs(direction)
+
+    acceleration = 0
+    throttle = controls.throttle
+    # Assume constant acceleration
+    if controls.boost:
+        acceleration = 2000
+    elif abs(throttle) > 0.015 and (direction*throttle > 0):
+        # not coasting and movement direction and throttle direction match
+        acceleration = 1000*direction_float_normalized*abs(throttle)
+    elif abs(throttle) > 0.015 and (direction*throttle < 0):
+        # not coasting but braking
+        acceleration = -3500*direction_float_normalized*abs(throttle)
+    elif abs(throttle) <= 0.015:
+        # this is fixed, not multiplied by throttle value
+        acceleration = -525*direction_float_normalized
+    
+    delta_v = acceleration*dt
+    original_v_mag = physics.velocity.length()
+    if controls.boost:
+        original_v_mag = min(original_v_mag, 2300)
+    else:
+        original_v_mag = min(original_v_mag, 1410)
+
+    physics.velocity = orientation.forward*(original_v_mag + delta_v)
+    delta_x = physics.velocity*dt
+    physics.location += delta_x
+
+    return physics
+
+    
+
--- src/simulation_human_test.py
+++ src/simulation_human_test.py
@@ -1,10 +1,22 @@
-from time import sleep
+"""
+Fake bot that takes controls from a controller.
+
+Code taken from https://github.com/robbai/RLBotSequence/tree/master/src and
+example code from pygame: https://www.pygame.org/docs/ref/joystick.html#pygame.joystick.Joystick
+"""
+
+from math import ceil
+from time import sleep, monotonic
 
 from rlbot.agents.base_script import BaseScript
 from rlbot.utils.game_state_util import Vector3, Rotator
 
 from util.orientation import Orientation, relative_location
+from util.vec import Vec3
+from car_simulation_by_controls import SimPhysics, step as carSimStep
+
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 def v3toUI(v3):
     return Vector3(v3.x, v3.y, v3.z)
@@ -50,6 +62,12 @@ class SimulationHumanTest(BaseAgent):
         self.axis_data = [0] * self.controller.get_numaxes()
         self.button_data = [False] * self.controller.get_numbuttons()
 
+        self.last_recorded_ts = 0
+        self.last_tick_ts = 0
+        self.physics: SimPhysics = None 
+
+        self.locations = []
+
     def update_controls(self, controls: SimpleControllerState):
         controls.throttle = -self.axis_data[2]
         controls.steer = self.axis_data[0]
@@ -59,39 +77,51 @@ class SimulationHumanTest(BaseAgent):
         controls.jump = self.button_data[0]
         controls.boost = self.button_data[1]
         controls.handbrake = self.button_data[2]
-    def get_output(self, packet):
-        offset = 100 
 
-        controls = SimpleControllerState()
+    def reset_physics(self, human):
+        print("Resetting")
+        self.locations = [Vec3(human.physics.location)]
+        self.physics = SimPhysics(
+            location=Vec3(human.physics.location),
+            velocity=Vec3(human.physics.velocity),
+            angular_velocity=Vec3(human.physics.angular_velocity),
+            rotation=human.physics.rotation)
 
-        human = packet.game_cars[0]
-        orientation = Orientation(human.physics.rotation)
-        offset_vec = orientation.forward * offset
-
-        loc = offset_vec + human.physics.location
+    def mark_simulation_spots(self):
+        # offset = 100 
+        # orientation = Orientation(self.physics.rotation)
+        # offset_vec = orientation.forward * offset
         self.renderer.begin_rendering()
-        self.renderer.draw_rect_3d(
-            loc, 8, 8, True, self.renderer.cyan(), centered=True
-        )
+        for i in range(len(self.locations)):
+            loc = self.locations[i]
+            component = float(i)/len(self.locations)
+            inverse = 1 - component
+            color = self.renderer.create_color(255,
+                ceil(255*component), 
+                ceil(255*inverse),
+                ceil(255*inverse)
+            )
+            self.renderer.draw_rect_3d(
+                loc, 4, 4, True, color, centered=True
+            )
         self.renderer.end_rendering()
-        # print(f"Location: {human.physics.location}")
-        # print(f"Rotation: {human.physics.rotation}")
-        # print(f"Velocity: {human.physics.velocity}")
-        # print(f"AngVelo : {human.physics.angular_velocity}")
+
+
+
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        human = packet.game_cars[0]
+        cur_ts = monotonic()
+
+        if self.physics is None:
+            self.reset_physics(human)
+            return SimpleControllerState()
+
+        self.mark_simulation_spots()
 
         # Get controls.
-        for event in pygame.event.get(): # User did something.
-            print("Getting controls", event)
-            # if event.type == pygame.JOYAXISMOTION:
-            #     self.axis_data[event.axis] = deadzone(
-            #         event.value, transform=event.axis > 3
-            #     )
-            # elif event.type == pygame.JOYBUTTONDOWN:
-            #     self.button_data[event.button] = True
-
-            # elif event.type == pygame.JOYBUTTONUP:
-            #     self.button_data[event.button] = False
-        
+        for _ in pygame.event.get(): # User did something.
+            pass
+       
         self.controller = pygame.joystick.Joystick(0)
         self.controller.init()
         for i in range(self.controller.get_numbuttons()):
@@ -102,9 +132,21 @@ class SimulationHumanTest(BaseAgent):
             axis = self.controller.get_axis(i)
             self.axis_data[i] = axis
 
+        if self.button_data[6]:
+            self.reset_physics(human)
+
         # Update controls.
+        controls = SimpleControllerState()
         self.update_controls(controls)
-        print("Sending controls", controls.throttle)
+        # print("Sending controls", controls.throttle)
+        tick_duration = cur_ts - self.last_tick_ts
+        carSimStep(self.physics, controls, tick_duration)
+        self.last_tick_ts = cur_ts
+
+        if cur_ts - self.last_recorded_ts > 0.1:
+            self.locations.append(Vec3(self.physics.location))
+            self.last_recorded_ts = cur_ts
+ 
         return controls
         # self.game_interface.update_player_input(controls, 0)
 
